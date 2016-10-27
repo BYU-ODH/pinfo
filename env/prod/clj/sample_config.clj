a27eefa8968a6e0992c9b7cc321c5ec263dcf5a9
Tory BYU <torys.anderson@gmail.com>
2016-10-27 16:27:12 -0600

    configs in place for profiles

---
+++ env/prod/clj/sample_config.clj
@@ -0,0 +1,6 @@
+;; This is a template that will need to be copied as pinfo.config with the appropriate values for your service. 
+(ns pinfo.sample-config)
+
+(def env {:client-id ""
+          :client-secret ""
+          :redirect-uri ""})
--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "service to return BYU preliminary ws info for a user"
   :url "https://github.com/BYU-ODH/pinfo"
   :min-lein-version "2.0.0"
+  :target-path "target/"
+  ;:main  ;; TODO
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
@@ -12,7 +14,7 @@
                  [ring/ring-json "0.4.0"]
                  [byu-odh/byu-ws "0.1.1"]]
   :plugins [[lein-ring "0.9.7"]
-            ;[lein-immutant "2.1.0"]
+                                        ;[lein-immutant "2.1.0"]
             ]
   :ring {:handler pinfo.handler/app}
   ;; :immutant {:war {:name "pid"
@@ -21,5 +23,34 @@
   ;;                  ;:destination "/home/torysa/Sites/wildfly-10.0.0.Final/standalone/deployments"
   ;;                  :context-path "/"}}
   :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+  {:uberjar {:omit-source true
+             :aot :all
+             :uberjar-name "pinfo.jar"
+             :source-paths ["env/prod/clj"]
+             :resource-paths ["env/prod/resources"]}
+
+   :dev           [:project/dev :profiles/dev]
+   :test          [:project/test :profiles/test]
+
+   :project/dev  {:dependencies [[prone "1.1.1"]
+                                 [javax.servlet/servlet-api "2.5"]
+                                 [ring/ring-mock "0.3.0"]
+                                 [ring/ring-devel "1.5.0"]
+                                 ;[pjstadig/humane-test-output "0.8.0"]
+                                 ;[doo "0.1.6"]
+                                 ;[binaryage/devtools "0.7.0"]
+                                 ]
+                  :plugins      [[com.jakemccrary/lein-test-refresh "0.14.0"]
+                                 ;[lein-doo "0.1.6"]
+                                 ]
+                  
+                  :doo {:build "test"}
+                  :source-paths ["env/dev/clj" "test/clj"]
+                  :resource-paths ["env/dev/resources"]
+                  :repl-options {:init-ns user}
+                  ;; :injections [(require 'pjstadig.humane-test-output)
+                  ;;              (pjstadig.humane-test-output/activate!)]
+                  }
+   :project/test {:resource-paths ["env/dev/resources" "env/test/resources"]}
+   :profiles/dev {}
+   :profiles/test {}})
