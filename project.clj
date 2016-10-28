1d4f94a8ce5938e8ea58e677c9c0bb49ad1ce118
Tory BYU <torys.anderson@gmail.com>
2016-10-28 11:27:57 -0600

    adding immutant dependency breaks build

--- project.clj
+++ project.clj
@@ -8,21 +8,20 @@
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
                  [ring-middleware-format "0.7.0"]
-                                        ;[cheshire "5.6.3"]
-                                        ;[org.immutant/web "2.1.0"]
+                 ;[org.immutant/web "2.1.0"]
                  [ring-server "0.4.0"]
                  [ring-cors "0.1.8"]
                  [ring/ring-json "0.4.0"]
                  [byu-odh/byu-ws "0.1.1"]]
   :plugins [[lein-ring "0.9.7"]
-                                        ;[lein-immutant "2.1.0"]
+            ;[lein-immutant "2.1.0"]
             ]
   :ring {:handler pinfo.handler/app}
-  ;; :immutant {:war {:name "pid"
-  ;;                  :main humplus-funding.core
-  ;;                  :destination "target/"
-  ;;                  ;:destination "/home/torysa/Sites/wildfly-10.0.0.Final/standalone/deployments"
-  ;;                  :context-path "/"}}
+  :immutant {:war {:name "pid"
+                   :main pinfo.core
+                   :destination "target/"
+                   ;:context-path "/"
+                   }}
   :profiles
   {:uberjar {:omit-source true
              :aot :all
